d7405d34215eb3a649f56144ed0e27a33ca31f96
amorphous786 <usamanadeem0786@gmail.com>
2023-01-13 21:45:04 +0500

    tested post api

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # url_shortner
 This is a practice project which takes a long url as payload and return a sweet and short url. FASTapi framework is used to make APIs and documentation of the project.
+
+
+to start server:uvicorn main:app --reload
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException
+import validators,schemas
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return "Welcom to the URL shortner API..."
+
+@app.post("/url")
+def create_url(url: schemas.URLBase):
+    if not validators.url(url.target_url):
+        raise HTTPException(status_code=400,detail="Not a URL")
+    return f"TODO: Create database entry for: {url.target_url}"
